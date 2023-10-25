9eb704583801a7beefc4e4cdb731af6e1dc36eba
supechicken <supechicken666@gmail.com>
2023-10-26 00:26:20 +0800

    Use `grep` to prevent bash null byte warnings

--- autostart/crew-sudo.sh
+++ autostart/crew-sudo.sh
@@ -1,4 +1,4 @@
-if [[ "$(< "/proc/$(ps -p $PPID -o ppid= | tr -d ' ')/cmdline")" == /sbin/frecon* ]]; then
+if grep -q '^/sbin/frecon' < "/proc/$(ps -p $PPID -o ppid= | tr -d ' ')/cmdline"; then
   # start crew-sudo daemon if running in VT-2
   [ -f /tmp/crew-sudo.socket ] || crew-sudo --daemon
 fi
