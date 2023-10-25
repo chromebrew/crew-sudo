459fa5072143bdc699d988ff0c8fa9de074d7770
supechicken <supechicken666@gmail.com>
2023-10-26 00:22:08 +0800

    Trim spaces printed by `ps`

--- autostart/crew-sudo.sh
+++ autostart/crew-sudo.sh
@@ -1,4 +1,4 @@
-if [[ "$(< "/proc/$(ps -p $PPID -o ppid=)/cmdline")" == /sbin/frecon* ]]; then
+if [[ "$(< "/proc/$(ps -p $PPID -o ppid= | tr -d ' ')/cmdline")" == /sbin/frecon* ]]; then
   # start crew-sudo daemon if running in VT-2
   [ -f /tmp/crew-sudo.socket ] || crew-sudo --daemon
 fi
