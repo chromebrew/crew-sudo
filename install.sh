cb2218c3f38027e8213578acb294d5a71c636c86
SupeChicken666 <supechicken666@gmail.com>
2024-01-17 01:13:14 +0800

    Fix installer path

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ ln -sf ../lib/crew-sudo/crew-sudo ${INSTALL_PREFIX}/bin/sudo
 if [ -n "${CREW_DEST_PREFIX}" ]; then
   # installing under chromebrew
   mkdir -p ${INSTALL_PREFIX}/etc/{env.d,bash.d}
-  ln -sf ../lib/crew-sudo/autostart/crew-sudo.sh ${INSTALL_PREFIX}/etc/env.d/crew_sudo
-  ln -sf ../lib/crew-sudo/autocomplete/crew-sudo.sh ${INSTALL_PREFIX}/etc/bash.d/crew_sudo
+  ln -sf ../../lib/crew-sudo/autostart/crew-sudo.sh ${INSTALL_PREFIX}/etc/env.d/crew_sudo
+  ln -sf ../../lib/crew-sudo/autocomplete/crew-sudo.sh ${INSTALL_PREFIX}/etc/bash.d/crew_sudo
 else
   # installing without chromebrew, append the autostart script to bashrc
   echo -e '\n'"source ${INSTALL_PREFIX}/lib/crew-sudo/autostart/crew-sudo.sh" >> ~/.bashrc
