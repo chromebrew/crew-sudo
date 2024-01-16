b37f381d43587a98c4a20c8a4ee4e7817e0ce546
SupeChicken666 <supechicken666@gmail.com>
2024-01-17 00:51:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,16 +11,19 @@ else
   : "${INSTALL_PREFIX:=/usr/local}"
 fi
 
+mkdir -p ${INSTALL_PREFIX}/lib
 cp -r . ${INSTALL_PREFIX}/lib/crew-sudo
 
+mkdir -p ${INSTALL_PREFIX}/bin
 ln -sf ../lib/crew-sudo/crew-sudo ${INSTALL_PREFIX}/bin/crew-sudo
 ln -sf ../lib/crew-sudo/crew-sudo ${INSTALL_PREFIX}/bin/sudo
 
 if [ -d ${INSTALL_PREFIX}/etc/env.d ]; then
   # installing under chromebrew
+  mkdir -p ${INSTALL_PREFIX}/etc/{env.d,bash.d}
   ln -sf ../lib/crew-sudo/autostart/crew-sudo.sh ${INSTALL_PREFIX}/etc/env.d/crew_sudo
   ln -sf ../lib/crew-sudo/autocomplete/crew-sudo.sh ${INSTALL_PREFIX}/etc/bash.d/crew_sudo
 else
   # installing without chromebrew, append the autostart script to bashrc
-  echo "source ${INSTALL_PREFIX}/lib/crew-sudo/autostart/crew-sudo.sh" > ~/.bashrc
+  echo "source ${INSTALL_PREFIX}/lib/crew-sudo/autostart/crew-sudo.sh" >> ~/.bashrc
 fi
